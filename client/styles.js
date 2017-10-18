f3cca0293bc7feb13f5d5a3869600e8b0d22eacc
Gerald Perriman <geraldperriman@gmail.com>
2017-10-17 21:28:27 -0400

    a

--- client/styles.js
+++ client/styles.js
@@ -1,186 +1,82 @@
 module.exports = {
 
   loginStyle: {
-    backgroundImage: "url('/img/gym.jpg')",
-    backgroundSize:"center center no-repeat",
-    border:"10px #DAA520 solid",
-    fontFamily:"Nova Flat",
-<<<<<<< HEAD
-    color:"#FF0000"
-  },
-  Card: {
-    color:"#DAA520",
-    fontFamily:"Nova Flat",
+    backgroundColor: "white"
   },
   signupStyle: {
-    backgroundImage: "url('/img/squat.jpg')",
-    textAlign:"center",
-    fontFamily:"Nova Flat",
-    color:"#FF0000",
-    backgroundSize:"45%",
-    backgroundRepeat:"round",
-    border:"10px #FF0000 solid",
-  },
-  headerStyle: {
-    paddingTop: 20,
-=======
-    color:"red"
-  },
-  signupStyle: {
-    backgroundColor: "#DAA520",
-    fontFamily:"Nova Flat",
+    backgroundColor: "white"
   },
   headerStyle: {
     paddingTop: 20
->>>>>>> 4c39c191a74fd5724a40197e9296676ea61d00f2
   },
   dashNavStyle: {
     paddingTop: 30,
-    paddingBottom: 20,
-    fontFamily:"Nova Flat",
-<<<<<<< HEAD
-    color:"#DAA520"
-=======
->>>>>>> 4c39c191a74fd5724a40197e9296676ea61d00f2
+    paddingBottom: 20
   },
   drawerStyle: {
-    backgroundColor:"black",
-    fontFamily:"Nova Flat",
+    backgroundImage: "url('img/background.jpg')"
   },
   linkStyle: {
-<<<<<<< HEAD
-    color: "#FF0000",
-=======
-    color: "red",
->>>>>>> 4c39c191a74fd5724a40197e9296676ea61d00f2
-    fontFamily: 'Nova Flat',
-    backgroundColor:"#DAA520",
+    color: "white",
+    fontFamily: 'Raleway',
     padding: 10
   },
   homepageStyle: {
-    fontFamily: 'Nova Flat',
-<<<<<<< HEAD
-    color:"#FF0000",
-    backgroundColor:"black",
-=======
-    color:"#DAA520",
-    backgroundColor:"black",
+    fontFamily: "Raleway",
     padding: 10,
     fontSize: 20
   },
-  buttonStyle: {
-    fontFamily: 'Nova Flat',
-    backgroundColor:"#DAA520",
-    color:"#FF0000",
-    border:"2px #FF0000 solid ",
->>>>>>> 4c39c191a74fd5724a40197e9296676ea61d00f2
-    padding: 10,
-    fontSize: 20,
-    marginLeft:"3em",
-  },
-  overlayStyle:{
-    backgroundColor:"#DAA520",
-    labelColor:"#DAA520",
-    fontFamily:"Nova Flat",
-    padding: 10,
-    fontSize: 20,
-  },
-  overlayStyleSignUp:{
-    backgroundColor:"#DAA520",
-    fill:"#FF0000",
-    fontFamily:"Nova Flat",
-    padding: 10
-  },
-  overlayStyleLoginIn:{
-    backgroundColor:"#FF0000",
-    color:"#DAA520",
-    fontFamily:"Nova Flat"
-  },
-  buttonStyle: {
-    fontFamily: 'Nova Flat',
-    backgroundColor:"#DAA520",
-    padding: 10,
-    fontSize: 20,
-  },
-  homepageStyleDiv: {
-    border:" 1em #DAA520 solid"
-  },
   profileTitleStyle: {
     textAlign: "center",
-    fontFamily: 'Nova Flat',
-<<<<<<< HEAD
-    color:"#FF0000"
-=======
-    backgroundColor:"#DAA520",
->>>>>>> 4c39c191a74fd5724a40197e9296676ea61d00f2
+    fontFamily: 'Raleway'
   },
   dropdownStyle: {
-    fontFamily: 'Nova Flat',
+    fontFamily: 'Raleway',
     fontSize: 20,
-    padding: 10,
+    padding: 10
   },
   paddingStyle: {
     paddingBottom: 10
   },
   routineName: {
-    fontFamily: 'Nova Flat',
+    fontFamily: 'Raleway',
     fontSize: 20,
-    textAlign: "center",
-<<<<<<< HEAD
-    color:"#DAA520"
-  },
-  selectTitle: {
-    fontFamily: 'Nova Flat',
-    textAlign: "center",
-    color:"#DAA520"
-=======
-    color:"red"
-  },
-  selectTitle: {
-    fontFamily: 'Nova Flat',
     textAlign: "center"
->>>>>>> 4c39c191a74fd5724a40197e9296676ea61d00f2
+  },
+  selectTitle: {
+    fontFamily: 'Raleway',
+    textAlign: "center"
   },
   cardTextStyle: {
-    fontFamily: 'Nova Flat',
+    fontFamily: 'Raleway',
     fontSize: 20,
     textAlign: "center",
-    padding: 20,
-<<<<<<< HEAD
-    color:"#DAA520"
+    padding: 20
   },
   logRoutineHeaderStyle: {
-    fontFamily: 'Nova Flat',
-    padding: 10,
-    color:"#DAA520"
-=======
-  },
-  logRoutineHeaderStyle: {
-    fontFamily: 'Nova Flat',
+    fontFamily: "Raleway",
     padding: 10
->>>>>>> 4c39c191a74fd5724a40197e9296676ea61d00f2
   },
   logRoutineComponentStyle: {
-    fontFamily: 'Nova Flat',
+    fontFamily: "Raleway",
     paddingLeft: 10,
-    paddingRight: 10,
-    color:"#DAA520"
+    paddingRight: 10
   },
   addRepComponentStyle: {
-    fontFamily: 'Nova Flat',
+    fontFamily: "Raleway",
     paddingLeft: 15,
     paddingTop: 15
   },
   progressDropDown: {
     position: "relative",
     left: 150,
-    fontFamily: 'Nova Flat',
+    fontFamily: 'Raleway',
     fontSize: 20
   },
   menuDropdown: {
-    fontFamily: 'Nova Flat',
+    fontFamily: 'Raleway',
     fontSize: 14,
-    padding: 5,
+    padding: 5
 
   }
 
